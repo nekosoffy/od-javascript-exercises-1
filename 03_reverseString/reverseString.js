0fbeb67fb9ec39312d28eee08fba924ce09750ae
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-03-25 19:06:37 -0300

    Solve exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arrayConversion = string.split("");
+    let length = arrayConversion.length;
+    let reversedString = ""
+    for (let i = 1; i <= length; ++i) {
+        reversedString += arrayConversion.pop();
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
