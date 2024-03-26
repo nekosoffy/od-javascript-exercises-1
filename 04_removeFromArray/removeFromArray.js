c521e463d7073315e8b49cdbeb10d07001fc96cf
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-03-26 14:23:30 -0300

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (let n = 0; n <= array.length - 1; ++n) {
+        for (let i = 0; i <= args.length - 1; ++i) {
+            if (array[n] === args[i]) {
+                array.splice(n, 1);
+                --n
+                break;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
