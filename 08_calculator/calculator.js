5cf80dcf65a6a897b1129ce1f372bdb4717955f2
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-03-31 15:17:58 -0300

    Solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+  for (let num of array) {
+    total += num;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  for (let num of array) {
+    total *= num;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+  return num ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  let total = num;
+  for (let i = num - 1; i >= 1; i--) {
+    total *= i;
+  }
+  return total;
 };
+
+console.log(factorial(3));
 
 // Do not edit below this line
 module.exports = {
