ac703c2d0a41cd452e5ed2cbe1932aad31febd87
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-04-01 14:25:21 -0300

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let cleanedArray = [];
+    let punctuation = '!?,.;´`~ '
+    let array = string.toLowerCase().split("");
+    array.forEach(item => {
+        if (!(punctuation.includes(item))) {
+            cleanedArray.push(item);
+        }
+    });
+    let cleanedString = cleanedArray.join("")
+    let reversedCleanedString = cleanedArray.reverse().join("");
+    return (cleanedString === reversedCleanedString);
 };
+
+console.log(palindromes("racecar"));
 
 // Do not edit below this line
 module.exports = palindromes;
