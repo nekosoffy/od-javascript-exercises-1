0f17fc7858f4ec93edeeb359048d52040f318276
nekosoffy <161766793+nekosoffy@users.noreply.github.com>
2024-04-05 18:49:44 -0300

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  ages = people.map((person) => 
+  (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth);
+  let oldestIndex = ages.indexOf(Math.max(...ages));
+  return people[oldestIndex];
 };
 
 // Do not edit below this line
